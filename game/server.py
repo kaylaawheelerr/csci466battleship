f95b8d4a32c5f7ad2b2a144b1f3a7ce020ee5e02
Cole Sluggett <cole.sluggett@onxmaps.com>
2019-09-20 14:37:19 -0600

    working

--- game/server.py
+++ game/server.py
@@ -1,30 +1,26 @@
-#!/usr/bin/env python3
+from http.server import HTTPServer, BaseHTTPRequestHandler
 
-import socket
-import urllib.parse
+from io import BytesIO
 
-HOST = '127.0.0.1'  # Standard loopback interface address (localhost)
-PORT = 65432        # Port to listen on (non-privileged ports are > 1023)
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.bind((HOST, PORT))
-    s.listen()
-    conn, addr = s.accept()
-    with conn:
-        print('Connected by', addr)
-        while True:
-            data = conn.recv(1024)
-            print(data)
-            data = data.decode('utf-8')
-            print(data)
-            data = urllib.parse.unquote(data)
-            print(data)
-            if not data:
-                break
-            conn.send(bytes('HTTP/1.1 200 OK \
-                \nDate: Mon, 27 Jul 2009 12: 28: 53 GMT \
-                \nServer: Apache/2.2.14 (Win32) \
-                \nLast-Modified: Wed, 22 Jul 2009 19: 15: 56 GMT \
-                \nContent-Length: 88 \
-                \nContent-Type: text/html \
-                \nConnection: Closed', 'utf-8'))
+class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
+
+    def do_GET(self):
+        self.send_response(200)
+        self.end_headers()
+        self.wfile.write(b'Hello, world!')
+
+    def do_POST(self):
+        content_length = int(self.headers['Content-Length'])
+        body = self.rfile.read(content_length)
+        self.send_response(200)
+        self.end_headers()
+        response = BytesIO()
+        response.write(b'This is POST request. ')
+        response.write(b'Received: ')
+        response.write(body)
+        self.wfile.write(response.getvalue())
+
+
+httpd = HTTPServer(('localhost', 8000), SimpleHTTPRequestHandler)
+httpd.serve_forever()
