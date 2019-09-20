8f985155dbe77f6836e73e82cc992d899fb4370a
Cmarquardt17 <christianmarquardt14@hotmail.com>
2019-09-20 17:00:43 -0600

    Update both py files

--- clientT.py
+++ clientT.py
@@ -1,23 +1,27 @@
-import requests
+#!/usr/bin/env python3
+
 import socket
-import json
-import sys
-local_hostname = socket.gethostname()
-ip_address = socket.gethostbyname(local_hostname)
-url = 'http://localhost:8000' 
-# # myobj = { "shot" : "test"}
-# url = 'http://httpbin.org/post' 
-myobj = { 'x' : sys.argv[1] , 'y' : sys.argv[2] }
-# test = { 'path' : "/apply.php"}
-# # x = requests.post(url, data = myobj,headers=myobj)
-# # x = requests.post(url,data=myobj,headers={'content-type': 'application/json'})
-# x = requests.post(url,json.dumps(myobj),headers=myobj)
-# y = requests.get(url)
-# print(x.text)
-# print(x.status_code)
-# print(y.form)
-# curl -d "foo=bar&bin=go" http://localhost:8080
-# requests.get(url,test)
-# requests.get(url, myobj)
-# print(requests.get(url,myobj))
-print(requests.post(url,myobj))
+import urllib.parse
+
+HOST = '127.0.0.1'  # The server's hostname or IP address
+PORT = 65432        # The port used by the server
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.connect((HOST, PORT))
+    m = 'POST / localhost:65432 HTTP/1.1\nHost: localhost:65432 \
+                    \nContent-Type: application/x-www-form-urlencoded \
+                    \nContent-Length: 64\n\nx = 1 & y = 2'
+    m = urllib.parse.quote(m)
+    m = bytes(m, 'utf-8')
+    print(m)
+    s.send(m)
+    data = s.recv(1024)
+
+print('Received', repr(data))
+
+# POST / test HTTP/1.1
+# Host: foo.example
+# Content-Type: application/x-www-form-urlencoded
+# Content-Length: 27
+
+# field1 = value1 & field2 = value2
--- server.py
+++ server.py
@@ -7,28 +7,46 @@ from io import BytesIO
 personal_board = r"/Users/christianmarquardt/Desktop/BattleShip/personal_board.txt"
 enemy_board = r"/Users/christianmarquardt/Desktop/BattleShip/enemy_board.txt"
 
-#fillBoard= open(personal_board,"w")
-#for i in range(10):
-#    fillBoard.write("__________\n")
-#fillBoard.close()
+
+#Counting a hit at the spot on the board if it is there
+def countHit(file, xCoord, yCoord):
+    board = open(file, "r")
+    s = board.readlines()
+    board.close()
+    return s[yCoord][xCoord]
 
 
+#checks the entire board for hits to see if there is a winner
+def countBoard(file):
+    numHits = 0
+    for i in range(10):
+        for j in range(10):
+            hit = countHit(file,i,j)
+            if hit == 'X' or hit =='O':
+                numHits +=1
+    return numHits
+
+
+#A simple print board method
 def print_board(file):
     board= open(file, "r")
     for i in board:
-        print("  ".join(i))
+        print(i)
     board.close()
 
 #Here will process the shot
 def shotTaken(xCoord,yCoord):
     xCoord = int(xCoord)
     yCoord = int(yCoord)
-    print("We are currently trying to fire at " + str(xCoord) + " " + str(yCoord))
+
     board = open(personal_board, "r")
-    print_board(personal_board)
     tempBoard = board.readlines()
     board.close()
-    if tempBoard[yCoord][xCoord] !='_' and tempBoard[yCoord][xCoord] != 'X' and tempBoard[yCoord][xCoord] != 'O':
+
+    print("Attempted to fire here " + str(xCoord) + " " + str(yCoord))
+
+    #This spot is a hit on a ship
+    if tempBoard[yCoord][xCoord] != '_' and tempBoard[yCoord][xCoord] != 'X' and tempBoard[yCoord][xCoord] != 'O':
         print("looks like we got a hit!")
         tempBoard[yCoord] = tempBoard[yCoord][0:xCoord]+ "X" + tempBoard[yCoord][xCoord+1:]
         board = open(personal_board, "w")
@@ -39,29 +57,53 @@ def shotTaken(xCoord,yCoord):
         print_board(personal_board)
 
     elif tempBoard[yCoord][xCoord] == "_":
-        print("Unsuccessful hit")
+        print("Shot and a miss!")
         tempBoard[yCoord] = tempBoard[yCoord][0:xCoord]+ "O" + tempBoard[yCoord][xCoord+1:]
         board = open(personal_board, "w")
 
         for i in tempBoard:
             board.write(i)
         board.close()
+        checkForSink(personal_board)
         print_board(personal_board)
 
-def countHit(file, x, y):
+def checkForSink(file):
+    counterCarrier = 0
+    counterBattleShip = 0
+    counterCruiser = 0
+    counterSubmarine = 0
+    counterDestroyer = 0
+    sinkList = ""
+
     board = open(file, "r")
     s = board.readlines()
     board.close()
-    return s[y][x]
-
-def countBoard(file):
-    numHits = 0
+    print("Sinks so far:")
     for i in range(10):
         for j in range(10):
-            hit = countHit(file,i,j)
-            if hit == 'X' or hit =='O':
-                numHits +=1
-    return numHits
+            if s[i][j] == "C":
+                counterCarrier += 1
+            elif s[i][j] == "B":
+                counterBattleShip += 1
+            elif s[i][j] == "R":
+                counterCruiser+= 1
+            elif s[i][j] == "S":
+                counterSubmarine += 1
+            elif s[i][j] == "D":
+                counterDestroyer += 1
+            else:
+                pass
+    if counterCarrier ==0:
+        sinklist = sinkList + "Carrier is sunk \n"
+    if counterBattleShip ==0:
+        sinklist = sinkList + "Battleship is sunk \n"
+    if counterCruiser ==0:
+        sinklist = sinkList + "Cruiser is sunk \n"
+    if counterSubmarine==0:
+        sinklist = sinkList + "Submarine is sunk \n"
+    if counterDestroyer ==0:
+        sinklist = sinkList + "Destroyer is sunk \n"
+    return sinkList
 
 class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
 
