91f6a3496a7fe0f90bd75cfdc240fa295b170b01
Cmarquardt17 <christianmarquardt14@hotmail.com>
2019-09-23 10:02:22 -0600

    SetupUpdate

--- game/server.py
+++ game/server.py
@@ -6,15 +6,15 @@ from http.server import HTTPServer, BaseHTTPRequestHandler
 from io import BytesIO
 
 
-def create_board(board_file):
-    f = open(board_file, "r")
-    if f.mode == 'r':
-        BOARD = f.read()
-        print(BOARD)
+# def create_board(board_file):
+#     f = open(board_file, "r")
+#     if f.mode == 'r':
+#         BOARD = f.read()
+#         print(BOARD)
 
 
 #default file paths being used
-personal_board = r"/Users/cole.sluggett/Documents/git/csci466battleship/game/own_board.txt"
+personal_board = r"/Users/christianmarquardt/Documents/GitHub/csci466battleship/game/own_board.txt"
 enemy_board = r"/Users/christianmarquardt/Documents/GitHub/csci466battleship/enemy_board.txt"
 
 
@@ -47,13 +47,22 @@ def print_board(file):
 
 
 def checkForInput(xCoord, yCoord):
-    if xCoord > 9 or xCoord < 0 and yCoord > 9 or yCoord < 0:
+    if xCoord > 9 and xCoord < 0 and yCoord > 9 and yCoord < 0:
         return [404, "HTTP Not Found"]
     else:
         return shotTaken(xCoord, yCoord)
 
 #Here will process the shot
-
+def sunkTest(file,letterSpot):
+    board = open(personal_board, "r")
+    tempBoard = board.readlines()
+    board.close()
+    for i in range(10):
+        for j in range(10):
+            if str(tempBoard[i][j]) == letterSpot:
+                return 0
+    sunkShip = str("hit=1\&sink=" + letterSpot)
+    return sunkShip
 
 def shotTaken(xCoord, yCoord):
     xCoord = int(xCoord)
@@ -68,6 +77,7 @@ def shotTaken(xCoord, yCoord):
     #This spot is a hit on a ship
     if tempBoard[yCoord][xCoord] != '_' and tempBoard[yCoord][xCoord] != 'X' and tempBoard[yCoord][xCoord] != 'O':
         print("looks like we got a hit!")
+        letterSpot = str(tempBoard[yCoord][xCoord])
         tempBoard[yCoord] = tempBoard[yCoord][0:xCoord] + \
             "X" + tempBoard[yCoord][xCoord+1:]
         board = open(personal_board, "w")
@@ -75,10 +85,10 @@ def shotTaken(xCoord, yCoord):
         for i in tempBoard:
             board.write(i)
         board.close()
-        sink = checkForSink(personal_board)
+        sink = sunkTest(board,letterSpot)
         print_board(personal_board)
-        if sink == "":
-            return 'hit'
+        if sink == "still alive":
+            return 1
         return sink
 
     elif tempBoard[yCoord][xCoord] == "_":
@@ -91,50 +101,50 @@ def shotTaken(xCoord, yCoord):
             board.write(i)
         board.close()
         print_board(personal_board)
-        return 'miss'
+        return 0
 
 
-def checkForSink(file):
-    counterCarrier = 0
-    counterBattleShip = 0
-    counterCruiser = 0
-    counterSubmarine = 0
-    counterDestroyer = 0
-    sinkList = ""
+# def checkForSink(file):
+#     counterCarrier = 0
+#     counterBattleShip = 0
+#     counterCruiser = 0
+#     counterSubmarine = 0
+#     counterDestroyer = 0
+#     sinkList = ""
 
-    board = open(file, "r")
-    s = board.readlines()
-    board.close()
-    print("Sinks so far:")
-    for i in range(10):
-        for j in range(10):
-            if s[i][j] == "C":
-                counterCarrier += 1
-            elif s[i][j] == "B":
-                counterBattleShip += 1
-            elif s[i][j] == "R":
-                counterCruiser += 1
-            elif s[i][j] == "S":
-                counterSubmarine += 1
-            elif s[i][j] == "D":
-                counterDestroyer += 1
-            else:
-                pass
-    if counterCarrier == 0:
-        sinklist = sinkList + "Carrier is sunk \n"
-    if counterBattleShip == 0:
-        sinklist = sinkList + "Battleship is sunk \n"
-    if counterCruiser == 0:
-        sinklist = sinkList + "Cruiser is sunk \n"
-    if counterSubmarine == 0:
-        sinklist = sinkList + "Submarine is sunk \n"
-    if counterDestroyer == 0:
-        sinklist = sinkList + "Destroyer is sunk \n"
-    return sinkList
+#     board = open(file, "r")
+#     s = board.readlines()
+#     board.close()
+#     print("Sinks so far:")
+#     for i in range(10):
+#         for j in range(10):
+#             if s[i][j] == "C":
+#                 counterCarrier += 1
+#             elif s[i][j] == "B":
+#                 counterBattleShip += 1
+#             elif s[i][j] == "R":
+#                 counterCruiser += 1
+#             elif s[i][j] == "S":
+#                 counterSubmarine += 1
+#             elif s[i][j] == "D":
+#                 counterDestroyer += 1
+#             else:
+#                 pass
+#     if counterCarrier == 0:
+#         sinklist = sinkList + "Carrier is sunk \n"
+#     if counterBattleShip == 0:
+#         sinklist = sinkList + "Battleship is sunk \n"
+#     if counterCruiser == 0:
+#         sinklist = sinkList + "Cruiser is sunk \n"
+#     if counterSubmarine == 0:
+#         sinklist = sinkList + "Submarine is sunk \n"
+#     if counterDestroyer == 0:
+#         sinklist = sinkList + "Destroyer is sunk \n"
+#     return sinkList
 
 
-BOARD = bytes()
-create_board(sys.argv[1])
+# BOARD = bytes()
+# create_board(sys.argv[1])
 
 class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
 
@@ -157,7 +167,6 @@ class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
         response.write(b'This is POST request. ')
         response.write(b'Received: ')
         self.wfile.write(response.getvalue())
-
 
 httpd = HTTPServer(('localhost', 8000), SimpleHTTPRequestHandler)
 httpd.serve_forever()
