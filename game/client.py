b93334117567a89cb440a4a94e6d2b217aed95e2
Dylan Lynn <dlynn237@gmail.com>
2019-09-24 21:18:19 -0600

    almost done

--- game/client.py
+++ game/client.py
@@ -5,7 +5,7 @@ import sys
 local_hostname = socket.gethostname()
 ip_address = socket.gethostbyname(local_hostname)
 url = 'http://localhost:8000' 
-myobj = { 'x' : sys.argv[1] , 'y' : sys.argv[2] }
+myobj = { 'x' : sys.argv[1] , 'y' : sys.argv[2] , 'z' : 0}
 response = (requests.post(url,myobj))
 print(response)
 if response.status_code == 200:
@@ -14,6 +14,7 @@ if response.status_code == 300:
     print("Miss! You suck!")
 if response.status_code == 350:
     print("Spot already chosen!")
+    print("HTTP Gone")
 if response.status_code == 400:
     print("You sunk a ship, nice job")
 if response.status_code == 420:
--- game/server.py
+++ game/server.py
@@ -11,10 +11,24 @@ import requests
 from io import BytesIO
 
 #default file paths being used
-personal_board = "own_board.txt"
-enemy_board = "enemy_board.txt"
-shots_taken = "shots_taken.txt"
-PORT = sys.argv[2]
+
+if sys.argv[2] == "own_board.txt":
+    personal_board = "own_board.txt"
+    enemy_board = "enemy_board.txt"
+    shots_taken = "shots_taken.txt"
+else:
+    personal_board = "enemy_board.txt"
+    enemy_board = "own_board.txt"
+    shots_taken = "opp_shots_taken.txt"
+
+PORT = int(sys.argv[1])
+
+if PORT == 8000:
+    PORT =8000
+    PORT2= 8001
+else:
+    PORT = 8001
+    PORT2 = 8000
 
 
 def checkWin(file):
@@ -65,8 +79,8 @@ def sunkTest(file,letterSpot):
 # 1st should update own board and enemy shot board. depending on which port was supplied.
 #This takes the shot coordinates and opens the file and replaces the string row
 def shotTaken(xCoord, yCoord):
-    myobj = { 'x' : xCoord , 'y' : yCoord }
-    url = 'localhost:'+PORT
+    myobj = { 'x' : xCoord , 'y' : yCoord , 'z' : 1}
+    url = 'http://localhost:'+str(PORT2)
     requests.post(url,myobj)
     xCoord = int(xCoord)
     yCoord = int(yCoord)
@@ -75,44 +89,69 @@ def shotTaken(xCoord, yCoord):
     tempBoard = board.readlines()
     board.close()
 
+    print("Attempted to fire here " + str(xCoord) + " " + str(yCoord))
+    if tempBoard[yCoord][xCoord] != '_' and tempBoard[yCoord][xCoord] != 'X' and tempBoard[yCoord][xCoord] != 'O': 
+        letterSpot = str(tempBoard[yCoord][xCoord])
+        print("looks like we got a hit!")
+        tempBoard[yCoord] = tempBoard[yCoord][0:xCoord] + \
+            "X" + tempBoard[yCoord][xCoord+1:]
+
+        board = open(personal_board, "w")
+
+
+        for i in tempBoard:
+            board.write(i)
+        board.close()
+       
+
+        #When we get a hit we want to check if we sunk the ship or not
+        sink = sunkTest(board,letterSpot)
+        checkForWin = checkWin(enemy_board)
+        if checkForWin  == "win":
+            return 3
+        elif sink == 1:
+            return 2
+        return 1
+
+    #Here would be a miss and write a O on that spot
+    elif tempBoard[yCoord][xCoord] == "_":
+        tempBoard[yCoord] = tempBoard[yCoord][0:xCoord] + \
+            "O" + tempBoard[yCoord][xCoord+1:]
+        print("Shot and a miss!")        
+        
+        board = open(personal_board, "w")
+
+        for i in tempBoard:
+            board.write(i)
+        board.close()
+        return 0
+    else:
+        print("There has been a shot here already")
+        return 350
+
+
+#only called from 3 var.
+def shot_board_write(xCoord,yCoord):
+    print("im here")
+    #enemy board
     board2 = open(enemy_board, "r")
     tempBoard2 = board2.readlines()
     board2.close()
-    
+    #shotboard
     board3 = open(shots_taken, "r")
     tempBoard3 = board3.readlines()
     board3.close()
 
 
     print("Attempted to fire here " + str(xCoord) + " " + str(yCoord))
-
+    
     #This spot is a hit on a ship
     if tempBoard2[yCoord][xCoord] != '_' and tempBoard2[yCoord][xCoord] != 'X' and tempBoard2[yCoord][xCoord] != 'O': 
-        # tempBoard[yCoord] = tempBoard[yCoord][0:xCoord] + \
-        #     "X" + tempBoard[yCoord][xCoord+1:]
-        letterSpot = str(tempBoard[yCoord][xCoord])
+        letterSpot = str(tempBoard2[yCoord][xCoord])
         print("looks like we got a hit!")
-        tempBoard2[yCoord] = tempBoard2[yCoord][0:xCoord] + \
-            "X" + tempBoard2[yCoord][xCoord+1:]
-        
         tempBoard3[yCoord] = tempBoard3[yCoord][0:xCoord] + \
             "X" + tempBoard3[yCoord][xCoord+1:]
 
-        # board = open(personal_board, "w")
-
-
-        # for i in tempBoard:
-        #     board.write(i)
-        # board.close()
-        board2 = open(enemy_board, "w")
-        for i in tempBoard2:
-            board2.write(i)
-        board2.close()
-        
-        # board3 = open(shots_taken, "r")
-        # board2 = open(enemy_board, "r")
-        # hit2 = board3.readlines()
-        # board3.close()
         board3 = open(shots_taken, "w")
         for i in tempBoard3:
             board3.write(i)
@@ -127,7 +166,7 @@ def shotTaken(xCoord, yCoord):
         # board2.close()
 
         #When we get a hit we want to check if we sunk the ship or not
-        sink = sunkTest(board,letterSpot)
+        sink = sunkTest(board3,letterSpot)
         checkForWin = checkWin(enemy_board)
         if checkForWin  == "win":
             return 3
@@ -146,20 +185,20 @@ def shotTaken(xCoord, yCoord):
         tempBoard3[yCoord] = tempBoard3[yCoord][0:xCoord] + \
             "O" + tempBoard3[yCoord][xCoord+1:]
         # board = open(personal_board, "w")
-        board2 = open(enemy_board, "w")
+        # board2 = open(enemy_board, "w")
         board3 = open(shots_taken,"w")
 
         # for i in tempBoard:
         #     board.write(i)
         # board.close()
 
-        for i in tempBoard2:
-            board2.write(i)
-        board2.close()
+        # for i in tempBoard2:
+        #     board2.write(i)
+        # board2.close()
         for i in tempBoard3:
             board3.write(i)
         board3.close()
-
+        print("woah fuck bud")
         return 0
     else:
         print("There has been a shot here already")
@@ -187,58 +226,65 @@ class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
         personal_string_board = boardWrite(personal_board)
         enemy_string_board = boardWrite(enemy_board)
         shot_string_board = boardWrite(shots_taken)
-
         self.send_response(200)
         self.end_headers()
         self.send_header('Content-type' , "text/html")
-        self.wfile.write(b"<html><h1>Personal Board</h1><style>.grid-container{display: flex;}.grid-item{width: 25px;}</style><body><h1 class = 'container'>"
+        if self.path == '/own_board.html':
+            self.wfile.write(b"<html><h1>Personal Board</h1><style>.grid-container{display: flex;}.grid-item{width: 25px;}</style><body><h1 class = 'container'>"
             +personal_string_board+ b"</h1></body></html>")
-        self.wfile.write(b"<html><h1>Shot Board</h1><style>.grid-container{display: flex;}.grid-item{width: 25px;}</style><body><h1 class = 'container'>"
+        elif self.path == '/opponent_board.html':
+            self.wfile.write(b"<html><style>.grid-container{display: flex;}.grid-item{width: 25px;}</style><body><h1 class = 'container'>"
+            +enemy_string_board+ b"</h1></body></html>")    
+        else:
+            self.wfile.write(b"<html><h1>Personal Board</h1><style>.grid-container{display: flex;}.grid-item{width: 25px;}</style><body><h1 class = 'container'>"
+            +personal_string_board+ b"</h1></body></html>")
+            self.wfile.write(b"<html><h1>Shot Board</h1><style>.grid-container{display: flex;}.grid-item{width: 25px;}</style><body><h1 class = 'container'>"
             +shot_string_board+ b"</h1></body></html>")
-        self.wfile.write(b"<html><h1>Enemy Board</h1><style>.grid-container{display: flex;}.grid-item{width: 25px;}</style><body><h1 class = 'container'>"
-            +enemy_string_board+ b"</h1></body></html>")
+        # self.wfile.write(b"<html><h1>Enemy Board</h1><style>.grid-container{display: flex;}.grid-item{width: 25px;}</style><body><h1 class = 'container'>"
+        #     +enemy_string_board+ b"</h1></body></html>")
 
     def do_POST(self): 
-        url = 'http://localhost:8000?'
+        url = 'http://localhost:'+PORT+'?'
         content = int(self.headers['Content-Length'])
-        post_data = self.rfile.read(content)
+        post_data = self.rfile.read(content)            
         post_data = post_data.decode("utf-8")
         print(url + str(post_data))
         coords = re.findall(r'\d+', post_data)
         print(coords)
         x = int(coords[0])
         y = int(coords[1])
-        return_message = checkForInput(x,y)
-
+        z = int(coords[2])
+        if z==1:
+            return_message = shot_board_write(x,y)
+        else:
+            return_message = checkForInput(x,y)
         #For a miss
         if return_message == 0:
-            print('Help miss')
             response = BytesIO()
-            response.write(b'http://localhost:8000?miss=0')
+            response.write(b'http://localhost:'+PORT+'?miss=0')
             self.send_response(300)
 
         #For a hit
         elif return_message == 1:
             print('Help hit')
             response = BytesIO()
-            response.write(b'http://localhost:8000?hit=1')
+            response.write(b'http://localhost:'+PORT+'?hit=1')
             self.send_response(200)
 
         #For a sink
         elif return_message == 2:
             response = BytesIO()
             aa = str(return_message)
-            sink_message = "http://localhost:8000?" + aa
+            sink_message = "http://localhost:"+PORT+"?" + aa
             sink_message = sink_message.encode()
             response.write(bytes(sink_message))
             self.send_response(400)
         elif return_message == 3:
-            print('Help win')
             response = BytesIO()
-            response.write(b'http://localhost:8000?win=3')
+            response.write(b'http://localhost:'+PORT+'?win=3')
             self.send_response(420)
         else:
-            print('Already taken')
+            response = BytesIO()
             self.send_response(350)
 
         self.end_headers()
@@ -247,6 +293,6 @@ class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
         response.write(b'Received: ')
         self.wfile.write(response.getvalue())
 
-httpd = HTTPServer(('localhost', 8000), SimpleHTTPRequestHandler)
+httpd = HTTPServer(('localhost',int(PORT)), SimpleHTTPRequestHandler)
 httpd.serve_forever()
 
